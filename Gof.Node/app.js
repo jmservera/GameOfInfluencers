dbed5cb25cce926a810e85a4adecb25cce1806eb
Juan Manuel Servera <jmservera@outlook.com>
2016-11-29 13:18:21 +0100

    Node with socket.io for realtime comm

--- Gof.Node/app.js
+++ Gof.Node/app.js
@@ -1,10 +1,35 @@
 var port = process.env.port || 1337;
 
-var app = require('express')();
+var express = require('express')
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+var timer=null;
 
 app.get('/', function (req, res) {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+io.on('connection', function (socket) {
+    console.log('a user connected: ' + socket.id);
+    if (timer === null) {
+        console.log("timer started");
+        var count = 0;
+        timer = setInterval(function () {
+            console.log("emit " + count);
+            io.emit("Message", "msg "+count++);
+        },1000);
+    }
+    socket.on('disconnect', function () {
+        console.log('user disconnected: ' + socket.id);
+        console.log('sockets: ' + io.sockets.connected);
+        //TODO: clear interval on listener
+    });
 });
 
 http.listen(port, function () {
